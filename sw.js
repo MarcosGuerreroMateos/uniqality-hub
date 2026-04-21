a23e7f3e40269f0f2537e57b735bfbc2fccfb711
MarcosGuerreroMateos <magu@fp.insjoaquimmir.cat>
2026-04-21 18:31:03 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Un Service Worker básico para que Chrome permita la instalación
+self.addEventListener('fetch', function(event) {
+  // Aquí podríamos hacer caché para que funcione sin internet
+});
